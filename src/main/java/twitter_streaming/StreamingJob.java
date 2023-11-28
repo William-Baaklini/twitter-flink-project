b7fefac6baee2349755b7aba5ef3b74ffee6a851
William-Baaklini <76127831+William-Baaklini@users.noreply.github.com>
2023-11-28 17:03:05 +0200

    Tried Adding docker

--- src/main/java/twitter_streaming/StreamingJob.java
+++ src/main/java/twitter_streaming/StreamingJob.java
@@ -1,62 +1,40 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 package twitter_streaming;
 
+import org.apache.flink.configuration.ConfigConstants;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.twitter.TwitterSource;
 
-/**
- * Skeleton for a Flink Streaming Job.
- *
- * <p>For a tutorial how to write a Flink streaming application, check the
- * tutorials and examples on the <a href="http://flink.apache.org/docs/stable/">Flink Website</a>.
- *
- * <p>To package your application into a JAR file for execution, run
- * 'mvn clean package' on the command line.
- *
- * <p>If you change the name of the main class (with the public static void main(String[] args))
- * method, change the respective entry in the POM.xml file (simply search for 'mainClass').
- */
+import java.util.Properties;
+
 public class StreamingJob {
 
-	public static void main(String[] args) throws Exception {
-		// set up the streaming execution environment
-		final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+	private static final String API_key = "yimjCtrm5I6T0UM29CLxHy2g1";
+	private static final String API_secret = "t0s0sHxMzPTDa9qmre3u12aChoC0xBKsED4ttkbXWre7Ialwl3";
+	private static final String Access_Token = "1729438742094524416-PJTiRXHZkpH23gUnB7pz8MQLMO0KSd";
+	private static final String Access_Token_secret = "BScDth9Hio1fUTtRxbJrKmifEcD3TWDCxFO8DAi6g1HFz";
 
-		/*
-		 * Here, you can start creating your execution plan for Flink.
-		 *
-		 * Start with getting some data from the environment, like
-		 * 	env.readTextFile(textPath);
-		 *
-		 * then, transform the resulting DataStream<String> using operations
-		 * like
-		 * 	.filter()
-		 * 	.flatMap()
-		 * 	.join()
-		 * 	.coGroup()
-		 *
-		 * and many more.
-		 * Have a look at the programming guide for the Java API:
-		 *
-		 * http://flink.apache.org/docs/latest/apis/streaming/index.html
-		 *
-		 */
+
+	public static void main(String[] args) throws Exception {
+
+		// set up the streaming execution environment
+//		Configuration config = new Configuration();
+//		config.setBoolean(ConfigConstants.LOCAL_START_WEBSERVER, true);
+//		final StreamExecutionEnvironment env = StreamExecutionEnvironment.createLocalEnvironmentWithWebUI(config);
+		StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+		env.setMaxParallelism(1);
+
+		Properties props = new Properties();
+		props.setProperty(TwitterSource.CONSUMER_KEY, API_key);
+		props.setProperty(TwitterSource.CONSUMER_SECRET, API_secret);
+		props.setProperty(TwitterSource.TOKEN, Access_Token);
+		props.setProperty(TwitterSource.TOKEN_SECRET, Access_Token_secret);
+		DataStream<String> tweetStream = env.addSource(new TwitterSource(props));
+
+		tweetStream.print();
 
 		// execute program
 		env.execute("Flink Streaming Java API Skeleton");
